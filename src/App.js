0347840bad91c2a5f8248bd94ac1e7d06f6cda3b
AlejandroLoutphi <alejandroloutphi@gmail.com>
2025-02-02 10:39:48 -0400

    Cambiar nombre a Vive Ávila

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import { initializeApp } from 'firebase/app';
 import './App.css';
 
-initializeApp({
-	apiKey: "AIzaSyCL6SvBfAg1kwaqCrOYy-2U2CoAiMuRsk8",
-	authDomain: "excursiones-avila.firebaseapp.com",
-	projectId: "excursiones-avila",
-	storageBucket: "excursiones-avila.firebasestorage.app",
-	messagingSenderId: "742115851793",
-	appId: "1:742115851793:web:11302cf4d26616cca1fbb0"
-});
+const firebaseConfig = {
+	apiKey: "AIzaSyCBfqXp33H3Zj_8GfzjHnxW4RIMC4F5ACc",
+	authDomain: "vive-avila.firebaseapp.com",
+	projectId: "vive-avila",
+	storageBucket: "vive-avila.firebasestorage.app",
+	messagingSenderId: "889941160937",
+	appId: "1:889941160937:web:ed10617ded750209689178"
+};
+
+const app = initializeApp(firebaseConfig);
 
 // Esto es solo un ejemplo copiado para testear
 function App() {
@@ -21,6 +23,7 @@ function App() {
 			<button onClick={() => setCount((count) => count + 1)}>
 				Increment
 			</button>
+			<iframe width="1460" height="610" frameborder="0" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=1460&amp;height=610&amp;hl=en&amp;q=Universidad%20Metropolitana%20de%20Caracas%20Caracas+(Universidad%20Metropolitana%20de%20Caracas)&amp;t=k&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=ea2f4b417aa47ec70adcc844a9ef3dc3a996e900'></script>
 		</>
 	);
 }
