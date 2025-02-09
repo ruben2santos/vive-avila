8fd149907170efe8895e7b247547bd432811cb40
AlejandroLoutphi <alejandroloutphi@gmail.com>
2025-02-09 09:42:22 -0400

    HTML foto de perfil

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const Page = Object.freeze({
 function Footer() {
 	return <footer>
 		<h1 className="footer_title">Vive Ávila</h1>
-		<h3 className="footer_text">Más información&nbsp;(+58)424-8014532</h3>
+		<h3 className="footer_text">Más información<br />(+58)424-8014532</h3>
 	</footer>
 }
 
@@ -41,50 +41,75 @@ function Register({ setPage, setUser }) {
 	const [formEmail, setFormEmail] = useState('');
 	const [formPassword, setFormPassword] = useState('');
 	const [formDate, setFormDate] = useState('2005-01-01');
+	const [formPfp, setFormPfp] = useState();
 
 	async function registerCreateAccount(e) {
 		e.preventDefault();
-		createUserWithEmailAndPassword(firebaseAuth, formEmail, formPassword).then((userCredential) => {
-			const user = userCredential.user;
-			setUser(user);
-			addDoc(collection(firebaseDb, "users"), {
-				uid: user.uid,
-				username: formUsername,
-				phone: formPhone,
-				email: formEmail,
-				date: formDate,
+		createUserWithEmailAndPassword(firebaseAuth, formEmail, formPassword)
+			.then((userCredential) => {
+				const user = userCredential.user;
+				setUser(user);
+				addDoc(collection(firebaseDb, "users"), {
+					uid: user.uid,
+					username: formUsername,
+					phone: formPhone,
+					email: formEmail,
+					date: formDate,
+				});
+			}).catch((e) => {
+				// TODO: handle firebase errors
+				console.log(e);
 			});
-		}).catch((e) => {
-			// TODO: handle firebase errors
-			console.log(e);
-		});
+	}
+
+	async function pfpChange(e) {
+		setFormPfp(URL.createObjectURL(e.target.files[0]));
+		// TODO: upload pfp (Cloud Firestore as String???)
 	}
 
 	return <>
 		<h1 className="register_title title">¡Únete a nuevas experiencias!</h1>
 		<form onSubmit={registerCreateAccount} className="register_form user_form">
 			<h2 className="register_subtitle subtitle">Registrar Usuario</h2>
-			<h3 className="register_form_text">Nombre Completo</h3>
-			<input type="text" id="register_name" name="register_name"
-				value={formUsername} onChange={(e) => setFormUsername(e.target.value)}
-				className="register_name register_field" required minLength="3" maxLength="40" />
+			<div className="register_form_section_name register_form_section">
+				<h3 className="register_form_text">Nombre Completo</h3>
+				<input type="text" id="register_name" name="register_name"
+					value={formUsername} onChange={(e) => setFormUsername(e.target.value)}
+					className="register_name register_field" required minLength="3" maxLength="40" />
+			</div>
 			{/* existe el type="tel", pero idk si lo queremos usar */}
-			<h3 className="register_form_text">Número de Teléfono</h3>
-			<input type="number" id="register_phone" name="register_phone"
-				value={formPhone} onChange={(e) => setFormPhone(e.target.value)}
-				className="register_phone register_field" required minLength="3" maxLength="40" />
-			<h3 className="register_form_text">Correo Electrónico</h3>
-			<input type="email" id="register_email" name="register_email"
-				value={formEmail} onChange={(e) => setFormEmail(e.target.value)}
-				className="register_email register_field" required minLength="3" maxLength="40" />
-			<h3 className="register_form_text">Contraseña</h3>
-			<input type="password" id="register_password" name="register_password"
-				value={formPassword} onChange={(e) => setFormPassword(e.target.value)}
-				className="register_password register_field" required minLength="6" maxLength="40" />
-			<h3 className="register_form_text">Fecha de Nacimiento</h3>
-			<input type="date" id="register_date" name="register_date"
-				value={formDate} onChange={(e) => setFormDate(e.target.value)}
-				className="register_date register_field" required max={new Date().toISOString().slice(0, 10)} />
+			<div className="register_form_section_phone register_form_section">
+				<h3 className="register_form_text">Número de Teléfono</h3>
+				<input type="number" id="register_phone" name="register_phone"
+					value={formPhone} onChange={(e) => setFormPhone(e.target.value)}
+					className="register_phone register_field" required minLength="3" maxLength="40" />
+			</div>
+			<div className="register_form_section_email register_form_section">
+				<h3 className="register_form_text">Correo Electrónico</h3>
+				<input type="email" id="register_email" name="register_email"
+					value={formEmail} onChange={(e) => setFormEmail(e.target.value)}
+					className="register_email register_field" required minLength="3" maxLength="40" />
+			</div>
+			<div className="register_form_section_password register_form_section">
+				<h3 className="register_form_text">Contraseña</h3>
+				<input type="password" id="register_password" name="register_password"
+					value={formPassword} onChange={(e) => setFormPassword(e.target.value)}
+					className="register_password register_field" required minLength="6" maxLength="40" />
+			</div>
+			<div className="register_form_section_date register_form_section">
+				<h3 className="register_form_text">Fecha de Nacimiento</h3>
+				<input type="date" id="register_date" name="register_date"
+					value={formDate} onChange={(e) => setFormDate(e.target.value)}
+					className="register_date register_field" required
+					max={new Date().toISOString().slice(0, 10)} />
+			</div>
+			<div className="register_form_section_pfp register_form_section">
+				<h3 className="register_form_text">Foto de Perfil</h3>
+				<input type="file" id="register_pfp" name="register_pfp" onChange={pfpChange}
+					className="register_date register_field" required accept="image/*" />
+				<img id="register_pfp_preview" name="register_pfp_preview"
+					className="register_pfp_preview pfp_preview" src={formPfp} />
+			</div>
 			<button type="submit" className="register_submit_button button_1">Crear Cuenta</button>
 		</form>
 		<h2 className="register_to_login">
@@ -99,25 +124,23 @@ function App() {
 	// Este es solo para que funcione el placeholder de Page.start
 	const [count, setCount] = useState(0);
 
-	const [user, setUser] = useState(null);
+	// Cambiar página defecto
 	const [page, setPage] = useState(Page.register);
+	const [user, setUser] = useState(null);
 
-	// Para mostrar una página, solo hacemos un switch sobre todos
-	// los páginas posibles y retornamos ese componente
+	// Para mostrar una página, solo hacemos un switch sobre todas
+	// las páginas posibles y retornamos ese componente
 	switch (page) {
-		case Page.login:
-		case Page.start:
-			// Placeholder
-			return (
-				<>
-					<h1>{count}</h1>
-					<button onClick={() => setCount((count) => count + 1)}>
-						Increment
-					</button>
-				</>
-			);
 		case Page.register:
 			return <Register setPage={setPage} setUser={setUser} />;
+		default:
+			// Placeholder
+			return <>
+				<h1>{count}</h1>
+				<button onClick={() => setCount((count) => count + 1)}>
+					Increment
+				</button>
+			</>;
 	}
 }
 
